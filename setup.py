f96477eeba5ae72f497fc364612415aa1809c387
teddyrendahl <trendahl@slac.stanford.edu>
2018-09-29 11:19:00 -0700

    STY: PEP8
    ./setup.py:5:11: E251 unexpected spaces around keyword / parameter equals
    ./setup.py:5:11: E221 multiple spaces before operator
    ./setup.py:5:17: E251 unexpected spaces around keyword / parameter equals
    ./setup.py:6:14: E251 unexpected spaces around keyword / parameter equals
    ./setup.py:6:14: E221 multiple spaces before operator
    ./setup.py:6:17: E251 unexpected spaces around keyword / parameter equals
    ./setup.py:7:15: E251 unexpected spaces around keyword / parameter equals
    ./setup.py:7:17: E251 unexpected spaces around keyword / parameter equals
    ./setup.py:8:14: E251 unexpected spaces around keyword / parameter equals
    ./setup.py:8:14: E221 multiple spaces before operator
    ./setup.py:8:17: E251 unexpected spaces around keyword / parameter equals
    ./setup.py:9:13: E251 unexpected spaces around keyword / parameter equals
    ./setup.py:9:13: E221 multiple spaces before operator
    ./setup.py:9:17: E251 unexpected spaces around keyword / parameter equals
    ./setup.py:10:15: E251 unexpected spaces around keyword / parameter equals
    ./setup.py:10:15: E221 multiple spaces before operator
    ./setup.py:10:20: E251 unexpected spaces around keyword / parameter equals
    ./setup.py:11:18: E251 unexpected spaces around keyword / parameter equals
    ./setup.py:11:20: E251 unexpected spaces around keyword / parameter equals
    ./setup.py:12:27: E251 unexpected spaces around keyword / parameter equals
    ./setup.py:12:29: E251 unexpected spaces around keyword / parameter equals

--- setup.py
+++ setup.py
@@ -2,12 +2,11 @@ import versioneer
 from setuptools import (setup, find_packages)
 
 
-setup(name     = 'lucid',
-      version  = versioneer.get_version(),
-      cmdclass = versioneer.get_cmdclass(),
-      license  = 'BSD-like',
-      author   = 'SLAC National Accelerator Laboratory',
-      packages    = find_packages(),
-      description = 'LCLS User Controls and Inteface Design',
-      include_package_data = True,
-    )
+setup(name='lucid',
+      version=versioneer.get_version(),
+      cmdclass=versioneer.get_cmdclass(),
+      license='BSD-like',
+      author='SLAC National Accelerator Laboratory',
+      packages=find_packages(),
+      description='LCLS User Controls and Inteface Design',
+      include_package_data=True)
