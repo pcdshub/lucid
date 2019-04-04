315f4bc0d688bf274a7e810964ff9ef735bfabd2
teddyrendahl <trendahl@slac.stanford.edu>
2019-04-04 16:36:47 -0700

    TST: LucidMainWindow initial tests

---
+++ tests/test_main_window.py
@@ -0,0 +1,38 @@
+import pytest
+from qtpy.QtCore import Qt
+from qtpy.QtWidgets import QDockWidget, QWidget
+
+from lucid import LucidMainWindow
+
+
+@pytest.fixture(scope='function')
+def main_window(qtbot):
+    main_window = LucidMainWindow()
+    qtbot.addWidget(main_window)
+    return main_window
+
+
+def test_add_multiple_docks(main_window):
+    first_dock = QDockWidget()
+    second_dock = QDockWidget()
+    main_window.addDockWidget(Qt.RightDockWidgetArea, first_dock)
+    assert main_window.dockWidgetArea(first_dock) == Qt.RightDockWidgetArea
+    assert first_dock in main_window._docks
+    main_window.addDockWidget(Qt.RightDockWidgetArea, second_dock)
+    assert main_window.dockWidgetArea(first_dock) == Qt.RightDockWidgetArea
+    assert main_window.dockWidgetArea(second_dock) == Qt.RightDockWidgetArea
+
+
+def test_main_window_find_window(main_window):
+    widget = QWidget()
+    dock = QDockWidget()
+    dock.setWidget(widget)
+    main_window.addDockWidget(Qt.RightDockWidgetArea, dock)
+    assert LucidMainWindow.find_window(widget) == main_window
+
+
+def test_main_window_find_window_with_orphan(qtbot):
+    widget = QWidget()
+    qtbot.addWidget(widget)
+    with pytest.raises(EnvironmentError):
+        LucidMainWindow.find_window(widget)
