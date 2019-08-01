a903ec439065c7030e11680de1fbc3c966e932cb
teddyrendahl <trendahl@slac.stanford.edu>
2019-08-01 10:44:32 -0700

    BLD: Use requirements.txt file in setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,20 @@ import versioneer
 from setuptools import (setup, find_packages)
 
 
+with open('requirements.txt') as f:
+    requirements = f.read().split()
+
+git_requirements = [r for r in requirements if r.startswith('git+')]
+requirements = [r for r in requirements if not r.startswith('git+')]
+print("User must install the following packages manually:\n" +
+      "\n".join(f' {r}' for r in git_requirements))
+
 setup(name='lucid',
       version=versioneer.get_version(),
       cmdclass=versioneer.get_cmdclass(),
       license='BSD-like',
       author='SLAC National Accelerator Laboratory',
+      install_requires=requirements,
       packages=find_packages(),
       description='LCLS User Controls and Inteface Design',
       include_package_data=True)
