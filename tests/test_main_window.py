72164606ad45bd6b20ab1232ff4d394e1142ad74
K Lauer <klauer@slac.stanford.edu>
2019-12-06 14:40:26 -0800

    TST/FIX: qtpydocking api -> pyqtads

--- tests/test_main_window.py
+++ tests/test_main_window.py
@@ -56,7 +56,7 @@ def test_main_window_in_dock(main_window, qtbot):
     widget = create_widget()
 
     assert main_window.dock_manager is not None
-    dock = main_window.dock_manager.find_dock_widget(widget_name)
+    dock = main_window.dock_manager.findDockWidget(widget_name)
     assert dock is not None
     assert dock.widget() == widget
 
@@ -71,18 +71,6 @@ def test_main_window_in_dock_with_orphan(qtbot):
 
     widget = create_widget()
     assert widget.isVisible()
-
-
-def test_main_window_in_dock_repeat(main_window, qtbot):
-    # Function to create show QWidget
-    widget = QWidget(parent=main_window)
-    qtbot.addWidget(widget)
-    create_widget = LucidMainWindow.in_dock(lambda: widget)
-    # Show widget once
-    create_widget()
-    dock = widget.parent()
-    create_widget()
-    assert dock == widget.parent()
 
 
 def test_main_window_in_dock_active_slot(main_window, qtbot):
