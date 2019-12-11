026cfa71fc61cc7edcbf11dfdfb7d774206bdbe0
Hugo Slepicka <slepicka@slac.stanford.edu>
2019-12-11 14:57:46 -0800

    ENH: First pass at adding some arguments at launcher.

--- lucid/launcher.py
+++ lucid/launcher.py
@@ -1,10 +1,44 @@
 def main():
+    import argparse
     from qtpy.QtWidgets import QApplication
     from ophyd.sim import SynAxis
     from random import randint
     import typhon
+    from . import __version__
     from .main_window import LucidMainWindow
     from .overview import IndicatorGrid
+
+    parser = argparse.ArgumentParser(description="LUCID - LCLS User Control and Interface Design")
+    parser.add_argument('--version', action='version',
+                    version='LUCID {version}'.format(version=__version__),
+                    help="Show LUCID's version number and exit.")
+
+    parser.add_argument(
+        'beamline',
+        help='Specify the beamline name to compose the home screen.',
+        type=str
+    )
+    parser.add_argument(
+        '--toolbar',
+        help='Path to the YAML file describing the entries for the Quick'+
+        ' Access Toolbar.',
+        default=None,
+        required=False,
+        type=argparse.FileType('r', encoding='UTF-8')
+    )
+    parser.add_argument(
+        '--row_group_key',
+        help='The Happi field to use for row grouping.',
+        default='location_group',
+        required=False
+    )
+    parser.add_argument(
+        '--col_group_key',
+        help='The Happi field to use for column grouping.',
+        default='functional_group',
+        required=False
+    )
+    lucid_args = parser.parse_args()
 
     app = QApplication([])
 
