17716e28d51a3eccc464c6a3bf15c1637154cd63
K Lauer <klauer@slac.stanford.edu>
2019-12-12 11:38:32 -0800

    ENH: add LUCID_DEBUG=1 option

--- demo/launch.sh
+++ demo/launch.sh
@@ -5,4 +5,8 @@ CONFIG=${CWD}/happi_demo.cfg
 TOOLBAR=${CWD}/example_toolbar.yml
 BEAMLINE=DEMO_BEAMLINE
 
-HAPPI_CFG=${CONFIG} lucid --toolbar=${TOOLBAR} ${BEAMLINE}
+if [ "$LUCID_DEBUG" == "1" ]; then
+    HAPPI_CFG=${CONFIG} ipython -i `which lucid` -- --toolbar=${TOOLBAR} ${BEAMLINE}
+else
+    HAPPI_CFG=${CONFIG} lucid --toolbar=${TOOLBAR} ${BEAMLINE}
+fi
