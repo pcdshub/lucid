d5320042ce74da5e0c825df3c482cbe49d673a67
Hugo Slepicka <slepicka@slac.stanford.edu>
2019-12-11 16:31:51 -0800

    ENH: Add log_level argument to lucid. ENH: Load data from Happi to fill grid with devices. (Left an option to specify beamline as DEMO to launch the fake devices).

--- lucid/launcher.py
+++ lucid/launcher.py
@@ -1,17 +1,30 @@
+def get_happi_entry_value(entry, key, search_extraneous=True):
+    extraneous = entry.extraneous
+    value = getattr(entry, key, None)
+    if value is None and search_extraneous:
+        # Try to look at extraneous
+        value = extraneous.get(key, None)
+
+    if not value:
+        raise ValueError('Invalid Key for Device.')
+    return value
+
+
 def main():
+    import logging
     import argparse
     from qtpy.QtWidgets import QApplication
-    from ophyd.sim import SynAxis
-    from random import randint
+    import happi
     import typhon
     from . import __version__
     from .main_window import LucidMainWindow
     from .overview import IndicatorGrid
 
-    parser = argparse.ArgumentParser(description="LUCID - LCLS User Control and Interface Design")
+    proj_desc = "LUCID - LCLS User Control and Interface Design"
+    parser = argparse.ArgumentParser(description=proj_desc)
     parser.add_argument('--version', action='version',
-                    version='LUCID {version}'.format(version=__version__),
-                    help="Show LUCID's version number and exit.")
+                        version='LUCID {version}'.format(version=__version__),
+                        help="Show LUCID's version number and exit.")
 
     parser.add_argument(
         'beamline',
@@ -20,8 +33,8 @@ def main():
     )
     parser.add_argument(
         '--toolbar',
-        help='Path to the YAML file describing the entries for the Quick'+
-        ' Access Toolbar.',
+        help='Path to the YAML file describing the entries for the Quick' +
+             ' Access Toolbar.',
         default=None,
         required=False,
         type=argparse.FileType('r', encoding='UTF-8')
@@ -38,23 +51,61 @@ def main():
         default='functional_group',
         required=False
     )
+    parser.add_argument(
+        '--log_level',
+        help='Configure level of log display',
+        choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'],
+        default='INFO'
+    )
+
     lucid_args = parser.parse_args()
+
+    logger = logging.getLogger('')
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter(
+        '[%(asctime)s] [%(levelname)-8s] - %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.setLevel(lucid_args.log_level)
+    handler.setLevel(lucid_args.log_level)
 
     app = QApplication([])
 
     window = LucidMainWindow()
     typhon.use_stylesheet(dark=True)
     grid = IndicatorGrid()
-    # Fill IndicatorGrid
-    for stand in ('DIA', 'DG1', 'TFS', 'DG2', 'TAB', 'DET', 'DG3'):
-        for system in ('Timing', 'Beam Control', 'Diagnostics', 'Motion',
-                       'Vacuum'):
-            # Create devices
-            device_count = randint(2, 20)
-            system_name = system.lower().replace(' ', '_')
-            devices = [SynAxis(name=f'{stand.lower()}_{system_name}_{i}')
-                       for i in range(device_count)]
-            grid.add_devices(devices, stand=stand, system=system)
+
+    if lucid_args.beamline != 'DEMO':
+        # Fill with Data from Happi
+        cli = happi.Client.from_config()
+        devices = cli.search(beamline=lucid_args.beamline)
+
+        for dev in devices:
+            try:
+                stand = get_happi_entry_value(dev, lucid_args.row_group_key)
+                system = get_happi_entry_value(dev, lucid_args.col_group_key)
+                dev_obj = [happi.loader.from_container(dev)]
+                grid.add_devices(dev_obj, stand=stand, system=system)
+            except ValueError as ex:
+                print(ex)
+                continue
+
+    else:
+        # Fill with random fake simulated devices
+        from random import randint
+        from ophyd.sim import SynAxis
+
+        # Fill IndicatorGrid
+        for stand in ('DIA', 'DG1', 'TFS', 'DG2', 'TAB', 'DET', 'DG3'):
+            for system in ('Timing', 'Beam Control', 'Diagnostics', 'Motion',
+                           'Vacuum'):
+                # Create devices
+                device_count = randint(2, 20)
+                system_name = system.lower().replace(' ', '_')
+                devices = [SynAxis(name=f'{stand.lower()}_{system_name}_{i}')
+                           for i in range(device_count)]
+                grid.add_devices(devices, stand=stand, system=system)
+
     window.setCentralWidget(grid)
     window.show()
 
