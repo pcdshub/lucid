43ba67c7ad8327129504470bded569cccba0279f
K Lauer <klauer@users.noreply.github.com>
2020-02-21 14:28:53 -0800

    Revert "TMP: a temporary patch for disabling floating on Linux"

--- lucid/main_window.py
+++ lucid/main_window.py
@@ -1,7 +1,6 @@
 import functools
 import logging
 import pathlib
-import sys
 
 import happi
 import typhos
@@ -301,10 +300,6 @@ class LucidMainWindow(QMainWindow):
             widget.setParent(dock)
             window.dock_manager.addDockWidget(
                 QtAds.RightDockWidgetArea, dock)
-
-            if sys.platform == 'linux':
-                # TODO: fix PyQtAds bug on Linux
-                dock.setFeature(dock.DockWidgetFloatable, False)
 
             # Ensure the main dock is actually visible
             widget.raise_()
