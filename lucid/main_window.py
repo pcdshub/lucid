5c2243a57dab6cf0a4564bb1c1117fa2e7c3e49e
K Lauer <klauer@slac.stanford.edu>
2020-02-21 09:41:59 -0800

    TMP: a temporary patch for disabling floating on Linux
    PyQtAds has a bug on Linux causing a segfault when floating any widget.

--- lucid/main_window.py
+++ lucid/main_window.py
@@ -1,6 +1,7 @@
 import functools
 import logging
 import pathlib
+import sys
 
 import happi
 import typhos
@@ -300,6 +301,10 @@ class LucidMainWindow(QMainWindow):
             widget.setParent(dock)
             window.dock_manager.addDockWidget(
                 QtAds.RightDockWidgetArea, dock)
+
+            if sys.platform == 'linux':
+                # TODO: fix PyQtAds bug on Linux
+                dock.setFeature(dock.DockWidgetFloatable, False)
 
             # Ensure the main dock is actually visible
             widget.raise_()
