eb5213c482dc1ac92ba68d1183b11be1c8b007b8
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-10-07 19:06:59 +0000

    Update docs after building Travis build 196 of pcdshub/lucid
    The docs were built from the tag 'master' against the commit
    b75a5ef403ab130210a700db6fc19a216f3db53b.
    
    The Travis build that generated this commit is at
    https://travis-ci.com/pcdshub/lucid/jobs/396538614.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.6.7/bin/doctr deploy --build-tags master --deploy-branch-name gh-pages --command '"doctr-versions-menu"'

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.7.0+6.g316397f',
+    VERSION: '0.7.1+2.gb75a5ef',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
