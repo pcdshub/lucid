e33c21dc5d3f995c6b66ac8fe80966e4b136bc85
Ken Lauer <klauer@slac.stanford.edu>
2021-07-20 11:50:14 -0700

    MNT: widget.screen() is in later versions

--- lucid/utils.py
+++ lucid/utils.py
@@ -235,21 +235,38 @@ def get_happi_device_cache():
 def screenshot_top_level_widgets():
     """Yield screenshots of all top-level widgets."""
     app = QApplication.instance()
-    for idx, widget in enumerate(app.topLevelWidgets()):
-        screenshot = widget.screen().grabWindow(widget.winId())
+    for screen_idx, screen in enumerate(app.screens(), 1):
+        logger.debug("Screen %d: %s %s", screen_idx, screen, screen.geometry())
+
+    primary_screen = app.primaryScreen()
+    logger.debug("Primary screen: %s", primary_screen)
+
+    def by_title(widget):
+        return widget.windowTitle() or str(id(widget))
+
+    index = 0
+    for widget in sorted(app.topLevelWidgets(), key=by_title):
+        if not widget.isVisible():
+            continue
+        screen = (
+            widget.screen()
+            if hasattr(widget, "screen")
+            else primary_screen
+        )
+        screenshot = screen.grabWindow(widget.winId())
         name = widget.windowTitle().replace(" ", "_")
-        suffix = ".{name}" if name else ""
-        yield f"{idx}{suffix}", screenshot
+        suffix = f".{name}" if name else ""
+        index += 1
+        yield f"{index}{suffix}", screenshot
 
 
 def save_all_screenshots(format="png") -> str:
     """Save screenshots of all top-level widgets to SCREENSHOT_DIR."""
     screenshot_id = str(uuid.uuid4())[:8]
     for name, screenshot in screenshot_top_level_widgets():
-        screenshot.save(
-            str(SCREENSHOT_DIR / f"{screenshot_id}.{name}.{format}"),
-            format
-        )
+        fn = str(SCREENSHOT_DIR / f"{screenshot_id}.{name}.{format}")
+        screenshot.save(fn, format)
+        logger.info("Saved screenshot: %s", fn)
     return screenshot_id
 
 
